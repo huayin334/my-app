54a77780c54fd21b26a91bdff0e1b9b7967768a9
qiuluo <qiuluo@wacai.com>
2021-02-26 15:23:59 +0800

    tab栏完善

--- client/src/components/Tabs/index.js
+++ client/src/components/Tabs/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.scss'
 import xiaoxi from './images/xiaoxi.png'
 import xiaoxi_active from './images/xiaoxi_active.png'
@@ -10,17 +10,24 @@ import wode from './images/wode.png'
 import wode_active from './images/wode_active.png'
 
 export default function Tabs() {
+  const [activenum, setActivenum] = useState(0)
   const list = [
     { name: '消息', icon: xiaoxi, icon_active: xiaoxi_active },
     { name: '好友', icon: dianhua, icon_active: dianhua_active },
     { name: '动态', icon: dingwei, icon_active: dingwei_active },
     { name: '我的', icon: wode, icon_active: wode_active },
   ]
+  const toDetail = (index) => {
+    setActivenum(index)
+  }
   return (
     <div className={'tab'}>
       {list.map((item, index) => (
-        <div key={item.name}>
-          <img src={item.icon}></img>
+        <div key={item.name} onClick={() => toDetail(index)}>
+          <img
+            src={index === activenum ? item.icon_active : item.icon}
+            alt={item.name}
+          ></img>
           {item.name}
         </div>
       ))}
