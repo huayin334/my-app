73ad6bcb060dfbea64c43e832aefca1aed54065d
qiuluo <qiuluo@wacai.com>
2021-02-26 17:56:31 +0800

    我的页面静态数据

--- client/src/pages/dynamic/index.js
+++ client/src/pages/dynamic/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import './index.scss'
 
 export default function Dynamic() {
-  return <div className="test">动态</div>
+  return <div className="test">朋友的动态 陌生人的动态</div>
 }
--- client/src/pages/mine/index.js
+++ client/src/pages/mine/index.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React from 'react'
+import './index.scss'
 
 export default function Mine() {
-  return <div>mine组件</div>;
+  return (
+    <div className="mine">
+      <div className="personal">
+        <img
+          src="https://tse2-mm.cn.bing.net/th/id/OIP.PioI7mJimr_s-zMY4bHYXwAAAA?pid=ImgDet&rs=1"
+          alt="加载失败>_<"
+        ></img>
+        <div className="">
+          <div>昵称:李华</div>
+          <div>年龄:22</div>
+          <div>地区:22</div>
+          <div>个性签名:我爱学习</div>
+        </div>
+      </div>
+      <div className="dynamic">
+        <div>
+          <div>我的动态</div>
+          <div>总动态数:32条</div>
+          <div>总点赞数:298个</div>
+          <div>{`>>>查看详情`}</div>
+        </div>
+      </div>
+    </div>
+  )
 }
